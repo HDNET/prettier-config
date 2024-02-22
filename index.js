071f1bb4c2c8a29e9f7997fe3611ac83677c152c
Thomas Völkel <thomas.voelkel@hdnet.de>
2024-02-22 11:32:45 +0100

    feat: Define default options explicitly

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 export default {
     arrowParens: 'avoid',
     bracketSameLine: false,
+    bracketSpacing: true,
     printWidth: 120,
+    tabWidth: 2,
     semi: false,
     singleQuote: true,
+    trailingComma: 'all',
+    useTabs: false,
     overrides: [
       {
         files: '*.json',
